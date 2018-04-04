badaeba18fed6bbcf27383db41d6039345511ccf
Linjie <linjiestyle@foxmail.com>
2018-04-04 17:46:29 +0800

    '第一次提交'

---
+++ projectPath/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <tabbar>
+    
+      <tabbar-item selected>
+        <img slot="icon" src="../static/icon/shouye.png">
+        <span slot="label">首页</span>
+      </tabbar-item>
+  
+      <tabbar-item>
+        <img slot="icon" src="../static/icon/shujia.png">
+        <span slot="label">书架</span>
+      </tabbar-item>
+      <tabbar-item  link="HelloWorld">
+        <img slot="icon" src="../static/icon/shufang.png">
+        <span slot="label">书房</span>
+      </tabbar-item>
+      <tabbar-item badge="">
+        <img slot="icon" src="../static/icon/fanxian.png">
+        <span slot="label">发现</span>
+      </tabbar-item>
+       <tabbar-item badge="">
+        <img slot="icon" src="../static/icon/wode.png">
+        <span slot="label">我的</span>
+      </tabbar-item>
+    </tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem, Group, Cell } from 'vux'
+
+export default {
+  components: {
+    Tabbar,
+    TabbarItem,
+    Group,
+    Cell
+  }
+}
+</script>
+
+<style type="text/css">
+	.weui-tabbar {
+		margin-left: -8px;
+	}
+		a {
+		text-decoration: none;
+		}
+</style>
---
+++ projectPath/src/components/page/index.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+
+	
+		<div>
+			123
+		</div>
+	
+
+</template>
+
+<script>
+
+import { Group, Cell } from 'vux'
+
+export default {
+  components: {
+    Grounp,
+    Cell
+  }
+}
+</script>
+
+<style lang="css">
+	
+</style>
